b31bdf6c0a244ba93cee418a63785e892e4e3b40
yuichi <yuichi.heguri18@gmail.com>
2020-12-11 22:09:20 +0900

    settings.py追加

---
+++ settings.py
@@ -0,0 +1,10 @@
+import os
+from os.path import join, dirname
+from dotenv import load_dotenv
+
+load_dotenv(verbose=True)
+
+dotenv_path = join(dirname(__file__), '.env')
+load_dotenv(dotenv_path)
+
+line_notify_token = os.environ.get("LINE_NOTIFY_TOKEN")
